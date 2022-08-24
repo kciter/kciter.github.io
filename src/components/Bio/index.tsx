963aaad88c5e211b5ff5350b5e41e61909df0c35
kciter <kciter@naver.com>
2022-08-24 23:27:42 +0900

    소개 수정

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -4,12 +4,12 @@ const Bio = () => {
   return (
     <div className="member-container">
       <div className="avatar">
-        <img src="/images/about/avatar.png" />
+        <img src="/images/about/avatar.jpg" />
       </div>
 
       <div className="summary">
         <div className="name">이선협</div>
-        <div className="description">뭐든지 다 잘하고 싶은 욕심많은 개발자</div>
+        <div className="description">그냥 개발자</div>
       </div>
     </div>
   );
