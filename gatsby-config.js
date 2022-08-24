85ef2b3f9e6fbc0974581a23b19964d5028f8128
kciter <kciter@naver.com>
2022-08-25 00:11:45 +0900

    rss 기능 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,5 +97,63 @@ module.exports = {
       },
     },
     `gatsby-plugin-cname`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.fields.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                });
+              });
+            },
+            query: `
+              {
+                allMdx(
+                  sort: { fields: [fields___date], order: DESC }
+                  filter: {
+                    fields: { type: { eq: "post" } }
+                    frontmatter: { draft: { ne: true } }
+                  }
+                ) {
+                  edges {
+                    node {
+                      excerpt(truncate: true)
+                      fields {
+                        date
+                        slug
+                      }
+                      frontmatter {
+                        title
+                        image
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/feed.xml",
+            title: "kciter.so",
+          },
+        ],
+      },
+    },
   ],
 };
